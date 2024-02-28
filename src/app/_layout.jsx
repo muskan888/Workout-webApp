c5e86c75d9c167a3693e0adb943ddd2a48682d50
muskan888 <dhingramuskan2001@gmail.com>
2024-02-28 13:57:53 -0500

    setup the graphql api

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,13 +1,12 @@
 //root layout
-import {Stack} from 'expo-router';
+import {Stack,Slot} from 'expo-router';
 export default function RootLayout(){
+    return <Slot/>
     return(
         <Stack> 
             <Stack.Screen name='index' options={{title:'Exercises'}}/>
 
-
-
-
         </Stack>
     )
 }
+
